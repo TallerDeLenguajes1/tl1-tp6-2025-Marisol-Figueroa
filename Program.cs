292ffe8378685f8d9394450d4c9d6ddc3f1aa8a3
marisolelbafigueroa@gmail.com <marisolelbafigueroa@gmail.com>
2025-05-25 01:08:27 -0300

    Ejercicio-01

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+int a;
+int b;
+a=10;
+b=a;
+Console.WriteLine("valor de a:"+a);
+Console.WriteLine("valor de b:"+b);
+
+//Ejercicio 01
+
+Console.Write("Ingrese un numero:");
+string? texto=Console.ReadLine();
+
+if(int.TryParse(texto,out int numero))
+{
+    if(numero > 0)
+    {
+     Console.WriteLine("El numero ingresado es:"+numero);
+    }
+    else
+    {
+      Console.WriteLine("El numero ingresado debe ser mayor que cero");
+    }
+
+}
+else
+{
+   Console.WriteLine("El numero no es un numero valido");
+}
+
